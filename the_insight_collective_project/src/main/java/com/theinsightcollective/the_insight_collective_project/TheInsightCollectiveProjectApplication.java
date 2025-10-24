1b102725725adebfc880106c7b5bee5694622cf8
Romina Trazzi <romina.trazzi@edu-its.it>
2025-10-24 10:31:38 +0200

    Added Java scaffolder

---
+++ the_insight_collective_project/src/main/java/com/theinsightcollective/the_insight_collective_project/TheInsightCollectiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.theinsightcollective.the_insight_collective_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheInsightCollectiveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheInsightCollectiveProjectApplication.class, args);
+	}
+
+}
---
+++ the_insight_collective_project/src/test/java/com/theinsightcollective/the_insight_collective_project/TheInsightCollectiveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theinsightcollective.the_insight_collective_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheInsightCollectiveProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
