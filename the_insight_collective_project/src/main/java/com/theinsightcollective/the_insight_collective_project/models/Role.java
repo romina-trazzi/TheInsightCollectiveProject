5b4df30e8ec6d6c6469b0641451d94d738646fe4
Romina Trazzi <romina.trazzi@edu-its.it>
2025-10-29 17:23:59 +0100

    Added role class

--- the_insight_collective_project/src/main/java/com/theinsightcollective/the_insight_collective_project/models/Role.java
+++ the_insight_collective_project/src/main/java/com/theinsightcollective/the_insight_collective_project/models/Role.java
@@ -1,5 +1,8 @@
+// Enum for user roles (USER/ADMIN).
+// Used for role-based access control in the application.
+
 package com.theinsightcollective.the_insight_collective_project.models;
 
-public class Role {
-    
+public enum Role {
+    USER, ADMIN;
 }
